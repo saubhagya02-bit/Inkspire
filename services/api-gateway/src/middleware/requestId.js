f610c840d4c06df679785db5996e9e8101ecb3af
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-05-27 15:45:46 +0530

    feat(middleware): add request ID middleware for distributed tracing across microservices

---
+++ services/api-gateway/src/middleware/requestId.js
@@ -0,0 +1,9 @@
+const { v4: uuidv4 } = require("uuid");
+
+const requestId = (req, res, next) => {
+  req.requestId = req.headers["x-request-id"] || uuidv4();
+  res.setHeader("X-Request-ID", req.requestId);
+  next();
+};
+
+module.exports = requestId;
