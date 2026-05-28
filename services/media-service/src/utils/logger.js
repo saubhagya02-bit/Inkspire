d74fdc060802bba51be2c2eaebf39955dcd33057
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-05-28 19:46:15 +0530

    chore(logger): configure Winston logger for structured service logging

---
+++ services/media-service/src/utils/logger.js
@@ -0,0 +1,15 @@
+const winston = require("winston");
+
+module.exports = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  defaultMeta: { service: "media-service" },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
