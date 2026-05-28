e963ad6e0673c24acebea8724dc7328b0543e436
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-05-28 12:41:13 +0530

    chore(db): update postgres-init.sql and add auth_db ALTER TABLE migration

--- infrastructure/docker/postgres-init.sql
+++ infrastructure/docker/postgres-init.sql
@@ -1,7 +1,14 @@
+-- Auth service database
 CREATE DATABASE auth_db;
 CREATE USER auth_user WITH PASSWORD 'auth_password';
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO auth_user;
 
+-- Post service database
 CREATE DATABASE post_db;
 CREATE USER post_user WITH PASSWORD 'post_password';
 GRANT ALL PRIVILEGES ON DATABASE post_db TO post_user;
+
+\c auth_db;
+
+ALTER TABLE users
+  ADD COLUMN IF NOT EXISTS two_factor_secret_pending VARCHAR(255);
